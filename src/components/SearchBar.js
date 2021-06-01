8d16237ddcd6f3237854001b5d7a069bfc09ac4d
Jmiller1294 <jayman1294@gmail.com>
2021-05-31 23:19:29 -0400

    added on InputChange and on FormSubmit function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  
+  state = { term: '' };
+ 
+ 
   onInputChange(event) {
-    console.log(event.target.value)
+    this.setState({ term: event.target.value })
+    console.log(this.state.term)
+  }
+
+  onFormSubmit = (event) =>{
+    event.preventDefault();
+    console.log(this.state.term)
   }
 
   render() {
     return(
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input 
+              type="text"
+              value={this.state.term} 
+              onChange={event => this.onInputChange(event)} 
+            />
           </div>
         </form>
       </div>
     ) 
   }
-}
+} 
 export default SearchBar;
