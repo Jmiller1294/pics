a742ce9f43c5cc03b805cf02d023a22a2075eb9b
Jmiller1294 <jayman1294@gmail.com>
2021-06-01 18:42:42 -0400

    created ImageList component

---
+++ src/components/ImageList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return  <img src={image.urls.regular} alt="searched pic"/>
+  })
+  return <div>{images}</div>;
+}
+export default ImageList;
